ef0659b8220bb38207d3ef9b82722fe0a0cf0f01
Brandon Howell <bshowell@calpoly.edu>
2023-08-05 16:27:50 -0700

    basic functionality in progress

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import AddRoutineScreen from './AddRoutineScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    return (
+        <PaperProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'My Routines' }} />
+                    <Stack.Screen name="AddRoutine" component={AddRoutineScreen} options={{ title: 'Add Routine' }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
+    );
+};
+
+export default App;
